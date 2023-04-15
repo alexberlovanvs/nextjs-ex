8cb3891ba76dc73c2e541efc4a80d018b7edb0e6
Alex Berlovan <berlo1989@gmail.com>
2023-04-15 18:26:34 +0300

    server-side

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
-// export async function getServerSideProps(context) {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-//   const data = await res.json();
+export async function getServerSideProps(context) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await res.json();
 
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
-export default function Home() {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-      const resData = await res.json();
-      setData(resData);
-    };
-    getData();
-  }, []);
+export default function Home({ data }) {
+  // const [data, setData] = useState({});
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  //     const resData = await res.json();
+  //     setData(resData);
+  //   };
+  //   getData();
+  // }, []);
 
   return (
     <>
