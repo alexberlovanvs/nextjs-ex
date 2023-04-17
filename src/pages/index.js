d8b9aea09b9c525f5410b739fa48a308f891283a
Alex Berlovan <berlo1989@gmail.com>
2023-04-17 13:54:34 +0300

    SERVER LIST

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,29 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 
-// export async function getServerSideProps(context) {
-//   // const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-//   // const data = await res.json();
+export async function getServerSideProps(context) {
+  // const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  // const data = await res.json();
 
-//   const resList = await fetch('https://jsonplaceholder.typicode.com/posts/');
-//   const dataList = await resList.json();
+  const resList = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  const dataList = await resList.json();
 
-//   return {
-//     props: {
-//       dataList,
-//     },
-//   };
-// }
+  return {
+    props: {
+      dataList,
+    },
+  };
+}
 
-export default function Home() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      const resList = await fetch('https://jsonplaceholder.typicode.com/posts/');
-      const dataList = await resList.json();
-      setData(dataList);
-    };
-    getData();
-  }, []);
-
+export default function Home({ dataList }) {
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const resList = await fetch('https://jsonplaceholder.typicode.com/posts/');
+  //     const dataList = await resList.json();
+  //     setData(dataList);
+  //   };
+  //   getData();
+  // }, []);
   const handleClick = (id) => {
     console.log(id);
   };
@@ -32,7 +31,7 @@ export default function Home() {
   return (
     <>
       <main>
-        {data.map((item) => {
+        {dataList.map((item) => {
           return (
             <div
               key={item.id}
