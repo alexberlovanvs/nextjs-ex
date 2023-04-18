4c956a4eda0cb1d5d9e92802f0a916b8b08c944a
Alex Berlovan <alexandru.berlovan@vspartners.us>
2023-04-18 22:02:20 +0300

    client interaction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export async function getServerSideProps(context) {
-  const resList = await fetch('https://jsonplaceholder.typicode.com/posts/');
-  const dataList = await resList.json();
-
-  return {
-    props: {
-      dataList,
-    },
-  };
-}
-
-export default function Home({ dataList }) {
+export default function Home() {
+  const [data, setData] = useState([]);
   const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    const getData = async () => {
+      const resList = await fetch('https://jsonplaceholder.typicode.com/posts/');
+      const dataList = await resList.json();
+      setData(dataList);
+    };
+    getData();
+  }, []);
+
   const handleClick = (id) => {
-    console.log(id);
     setCounter(counter + 1);
   };
 
   return (
     <>
       <main>
-        {dataList.map((item) => {
+        {data.map((item) => {
           return (
             <div
               key={item.id}
